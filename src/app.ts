71fb02e72075c896e584d800fc2c84208bc48478
Rayhan <mdrayhan0474@gmail.com>
2026-03-25 06:10:22 +0600

    Add initial backend source folder

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Application, Request, Response } from "express";
+// import { prisma } from "./app/lib/prisma";
+// import { IndexRoutes } from "./app/routes";
+
+const app: Application = express();
+
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+// app.use("/api/v1", IndexRoutes);
+
+// Basic route
+// app.get('/', async (req: Request, res: Response) => {
+
+//     const specialty = await prisma.specialty.create({
+//         data: {
+//             title: 'Cardiology'
+//         }
+//     })
+//     res.status(201).json({
+//         success: true,
+//         message: 'API is working',
+//         data: specialty
+//     })
+// });
+
+export default app;
