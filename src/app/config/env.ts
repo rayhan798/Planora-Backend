9bcc1383758b9f527c179155d9568078decd8b45
Rayhan <mdrayhan0474@gmail.com>
2026-03-29 08:24:09 +0600

    add enveriable

---
+++ src/app/config/env.ts
@@ -0,0 +1,105 @@
+import dotenv from 'dotenv';
+import status from 'http-status';
+import AppError from '../errorHelpers/AppError';
+
+dotenv.config();
+
+interface EnvConfig {
+    NODE_ENV: string;
+    PORT: string;
+    DATABASE_URL: string;
+    ACCESS_TOKEN_SECRET: string;
+    REFRESH_TOKEN_SECRET: string;
+    ACCESS_TOKEN_EXPIRES_IN: string;
+    REFRESH_TOKEN_EXPIRES_IN: string;
+    EMAIL_SENDER:{
+        SMTP_USER: string;
+        SMTP_PASS: string;
+        SMTP_HOST: string;
+        SMTP_PORT: string;
+        SMTP_FROM: string;
+    }
+   
+    FRONTEND_URL: string;
+    CLOUDINARY:{
+        CLOUDINARY_CLOUD_NAME: string;
+        CLOUDINARY_API_KEY: string;
+        CLOUDINARY_API_SECRET: string;
+    },
+    STRIPE:{
+        STRIPE_SECRET_KEY: string;
+        STRIPE_WEBHOOK_SECRET: string;
+    },
+    SUPER_ADMIN_EMAIL: string;
+    SUPER_ADMIN_PASSWORD: string;
+}
+
+
+const loadEnvVariables = (): EnvConfig => {
+
+    const requireEnvVariable = [
+        'NODE_ENV',
+        'PORT',
+        'DATABASE_URL',
+      
+        'ACCESS_TOKEN_SECRET',
+        'REFRESH_TOKEN_SECRET',
+        'ACCESS_TOKEN_EXPIRES_IN',
+        'REFRESH_TOKEN_EXPIRES_IN',
+        
+        'EMAIL_SENDER_SMTP_USER',
+        'EMAIL_SENDER_SMTP_PASS',
+        'EMAIL_SENDER_SMTP_HOST',
+        'EMAIL_SENDER_SMTP_PORT',
+        'EMAIL_SENDER_SMTP_FROM',
+        'FRONTEND_URL',
+        'CLOUDINARY_CLOUD_NAME',
+        'CLOUDINARY_API_KEY',
+        'CLOUDINARY_API_SECRET',
+        'STRIPE_SECRET_KEY',
+        'STRIPE_WEBHOOK_SECRET',
+        'SUPER_ADMIN_EMAIL',
+        'SUPER_ADMIN_PASSWORD',
+    ]
+
+    requireEnvVariable.forEach((variable) => {
+        if (!process.env[variable]) {
+            // throw new Error(`Environment variable ${variable} is required but not set in .env file.`);
+            throw new AppError(status.INTERNAL_SERVER_ERROR, `Environment variable ${variable} is required but not set in .env file.`);
+        }
+    })
+
+    return {
+        NODE_ENV: process.env.NODE_ENV as string,
+        PORT: process.env.PORT as string,
+        DATABASE_URL: process.env.DATABASE_URL as string,
+       
+        ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+        REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+        ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+        REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+       
+        EMAIL_SENDER: {
+            SMTP_USER: process.env.EMAIL_SENDER_SMTP_USER as string,
+            SMTP_PASS: process.env.EMAIL_SENDER_SMTP_PASS as string,
+            SMTP_HOST: process.env.EMAIL_SENDER_SMTP_HOST as string,
+            SMTP_PORT: process.env.EMAIL_SENDER_SMTP_PORT as string,
+            SMTP_FROM: process.env.EMAIL_SENDER_SMTP_FROM as string,
+        },
+      
+        FRONTEND_URL: process.env.FRONTEND_URL as string,
+        CLOUDINARY: {
+            CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+            CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+            CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+        },
+        STRIPE: {
+            STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY as string,
+            STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET as string,
+        },
+        SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+    }
+}
+
+export const envVars = loadEnvVariables();
