bf71f1b89e5127ebd5a6ff5507d449a4661cbaa3
Rayhan <mdrayhan0474@gmail.com>
2026-04-24 22:11:53 +0600

    feat: implement jwt

---
+++ src/app/utils/jwt.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+
+const createToken = (payload: JwtPayload, secret: string, options: SignOptions) => {
+  return jwt.sign(payload, secret, options); // jwt.sign ব্যবহার
+};
+
+const verifyToken = (token: string, secret: string) => {
+  try {
+    const decoded = jwt.verify(token, secret) as JwtPayload;
+    return {
+      success: true,
+      data: decoded,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+      error,
+    };
+  }
+};
+
+const decodeToken = (token: string) => {
+  return jwt.decode(token) as JwtPayload;
+};
+
+export const jwtUtils = {
+  createToken,
+  verifyToken,
+  decodeToken,
+};
